6101fdd6ce5152c22d171d844e71ee6466b3be01
dinesh.gupta <dineshg009@gmail.com>
2023-11-03 12:25:33 +0530

    Student service for all student view  created

--- src/main/java/com/SchoolMgmt/Controller/HomePage.java
+++ src/main/java/com/SchoolMgmt/Controller/HomePage.java
@@ -1,13 +1,25 @@
 package com.SchoolMgmt.Controller;
 
+import com.SchoolMgmt.Entity.Student;
+import com.SchoolMgmt.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomePage {
 
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping("/index")
-    public String homepage(){
+    public String homepage(Model model){
+
+        List<Student> students=studentService.getAllStudent();
+        model.addAttribute("student", students);
         return "index";
     }
 
